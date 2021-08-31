c18125a2571e0c7709b59cab05ed518294795b4a
HyeongYeon Ham <guddus326@naver.com>
2021-08-31 10:59:40 +0900

    인덱서 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -6,27 +6,51 @@ using System.Threading.Tasks;
 
 namespace CSBasic7
 {
-    class Wanted<T>
-        //where T:Student
+    class Products
     {
-        public T Value;
-        public Wanted(T value)
+        private List<string> list=new List<string>();
+        public Products()
         {
-            this.Value = value;
+            list.Add("짜장면");
+            list.Add("짬뽕");
+            list.Add("탕수육");
+            list.Add("볶음밥");
+            list.Add("마파두부");
+            list.Add("칠리새우");
+        }
+
+        public string this[int i]{
+            get { return list[i]; }
+            set { Console.WriteLine(i + "번째 상품 등록"); }
         }
     }
-    class Student
-    {
-        public override string ToString()
-        {
-            return "학생";
-        }
-    }
+   
 
     class Program
     {
+        class Wanted<T>
+        //where T:Student
+        {
+            public T Value;
+            public Wanted(T value)
+            {
+                this.Value = value;
+            }
+        }
+        class Student
+        {
+            public override string ToString()
+            {
+                return "학생";
+            }
+        }
         static void Main(string[] args)
         {
+            Products ps = new Products();
+            Random rs = new Random();
+            int i = rs.Next(0, 5);
+            Console.WriteLine(ps[i]);
+
             Wanted<string> ws = new Wanted<string>("String");
             Wanted<int> wi = new Wanted<int>(42);
             Wanted<Student> wstu = new Wanted<Student>(new Student());
