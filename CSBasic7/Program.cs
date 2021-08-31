7734111345684bfbb6540b792e7abd48880a1c9b
HyeongYeon Ham <guddus326@naver.com>
2021-08-31 10:52:01 +0900

    제네릭 예제

--- CSBasic7/Program.cs
+++ CSBasic7/Program.cs
@@ -6,11 +6,34 @@ using System.Threading.Tasks;
 
 namespace CSBasic7
 {
+    class Wanted<T>
+        //where T:Student
+    {
+        public T Value;
+        public Wanted(T value)
+        {
+            this.Value = value;
+        }
+    }
+    class Student
+    {
+        public override string ToString()
+        {
+            return "학생";
+        }
+    }
 
     class Program
     {
         static void Main(string[] args)
         {
+            Wanted<string> ws = new Wanted<string>("String");
+            Wanted<int> wi = new Wanted<int>(42);
+            Wanted<Student> wstu = new Wanted<Student>(new Student());
+
+            Console.WriteLine(ws.Value);
+            Console.WriteLine(wi.Value);
+            Console.WriteLine(wstu.Value);
         }
     }
 }
