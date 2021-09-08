e7e4336c7decc190538b6881b76261d023a2d8fe
HyeongYeon Ham <guddus326@naver.com>
2021-09-08 10:09:15 +0900

    콤보박스, 리스트박스 데이터 바인딩 연동 및 이벤트 연결 예제

--- CSBasic9-combobox/Form1.cs
+++ CSBasic9-combobox/Form1.cs
@@ -15,6 +15,46 @@ namespace CSBasic9_combobox
         public Form1()
         {
             InitializeComponent();
+            foodBindingSource.Add(new Food() {Name="삼선해물볶음짬뽕",Price=8500 });
+            foodBindingSource.Add(new Food() { Name = "삼선불짜장", Price = 8500 });
+            foodBindingSource.Add(new Food() { Name = "굴짬뽕", Price = 8000 });
+            foodBindingSource.Add(new Food() { Name = "육개장", Price = 6500 });
+            foodBindingSource.Add(new Food() { Name = "짬짜면", Price = 7000 });
+            foodBindingSource.Add(new Food() { Name = "탕짜면", Price = 8000 });
+
+            comboBox1.SelectedIndexChanged+=DataSelect;
+
+            foodBindingSource1.Add(new Food() { Name = "아메리카노", Price = 4100 });
+            foodBindingSource1.Add(new Food() { Name = "카페라떼", Price = 4600 });
+            foodBindingSource1.Add(new Food() { Name = "카푸치노", Price = 6500 });
+            foodBindingSource1.Add(new Food() { Name = "오트밀 라떼", Price = 5100 });
+            foodBindingSource1.Add(new Food() { Name = "카페모카", Price = 5200 });
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+
+
+
+        }
+
+        private void DataSelect(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            Food food = cb.SelectedItem as Food;
+            if(food != null)
+            {
+                MessageBox.Show(cb.SelectedValue.ToString());
+                MessageBox.Show(food.Name + " : " + food.Price);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox lb = sender as ListBox;
+            Food food = lb.SelectedItem as Food;
+            if (food != null)
+            {
+                MessageBox.Show(lb.SelectedValue.ToString());
+                MessageBox.Show(food.Name + " : " + food.Price);
+            }
         }
     }
 }
