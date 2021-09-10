b171d89bf92bc9d8b7f9e93d85840386b2ac7eea
HyeongYeon Ham <guddus326@naver.com>
2021-09-10 10:02:26 +0900

    delegator (메서드 이름) 방식

--- CSBasic10/Program.cs
+++ CSBasic10/Program.cs
@@ -8,8 +8,36 @@ namespace CSBasic10
 {
     class Program
     {
+        class Product
+        {
+            public string Name { get; set; }
+            public int Price { get; set; }
+
+        }
         static void Main(string[] args)
         {
+            List<Product> products = new List<Product>()
+            {
+                new Product(){Name="감자", Price=500},
+                new Product(){Name="사과", Price=400},
+                new Product(){Name="배추", Price=300},
+                new Product(){Name="상추", Price=200},
+                new Product(){Name="고구마", Price=600}
+            };
+            //정렬
+            products.Sort(SortWithPrice);
+
+
+            //출력
+            foreach(var item in products)
+            {
+                Console.WriteLine(item.Name + " : " + item.Price);
+            }
+        }
+
+        private static int SortWithPrice(Product x, Product y)
+        {
+            return x.Price.CompareTo(y.Price);
         }
     }
 }
