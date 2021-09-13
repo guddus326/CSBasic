a1f41b14f3d376c6a953dc6bc04a5443c1e1f159
HyeongYeon Ham <guddus326@naver.com>
2021-09-13 14:42:19 +0900

    Delegate 연산

--- CSBasic10/Program.cs
+++ CSBasic10/Program.cs
@@ -8,6 +8,16 @@ namespace CSBasic10
 {
     class Program
     {
+        public delegate void SendString(string message);
+        public static void Hello(string message)
+        {
+            Console.WriteLine("안녕하세요" + message + "씨!!");
+        }
+        public static void GoodBye(string message)
+        {
+            Console.WriteLine("안녕히계세요" + message + "씨!!");
+        }
+
         class Product
         {
             public string Name { get; set; }
@@ -16,6 +26,19 @@ namespace CSBasic10
         }
         static void Main(string[] args)
         {
+            SendString sayHello, sayGoodbye, multiDelegate;
+            sayHello = Hello;
+            sayGoodbye = GoodBye;
+
+            multiDelegate = sayHello + sayGoodbye;
+            multiDelegate += sayHello;
+            multiDelegate("함형연");
+            Console.WriteLine();
+            multiDelegate -= sayGoodbye;
+            multiDelegate("함형연");
+
+
+
             List<Product> products = new List<Product>()
             {
                 new Product(){Name="감자", Price=500},
